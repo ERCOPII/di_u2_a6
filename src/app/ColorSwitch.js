bfa230af441ac2612d0e3d6dc9dc3c672960223d
ERCOPII <miguelangelealdelojos@gmail.com>
2024-11-06 12:17:50 +0100

    Ejercicio Completado

--- src/app/ColorSwitch.js
+++ src/app/ColorSwitch.js
@@ -1,9 +1,8 @@
-export default function ColorSwitch({
-    onChangeColor
-  }) {
-    return (
-      <button>
-        Cambiar color
-      </button>
-    );
-  }
+export default function ColorSwitch({ onChangeColor }) {
+  return (
+    <button onClick={onChangeColor}>
+      Cambiar color
+    </button>
+  );
+}
+  
